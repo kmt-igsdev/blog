cfd187534895a322ddd150d14e0c52dfeeceebe6
Kyaw Min Thu <kmt.igsdeveloper@gmail.com>
2022-12-30 15:34:24 +0630

    add: email queue job testing

---
+++ resources/views/emails/testMail.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 8 Send Mail using Queue Tutorial - TechvBlogs</title>
+</head>
+<body>
+    <p>Hi Testing</p>
+    <p>This is test queue mail.</p>
+    <p>Please take a look.</p>
+    <strong>Thank you</strong>
+</body>
+</html>
